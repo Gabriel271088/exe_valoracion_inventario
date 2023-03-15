dcb1d2c4ced914371f83be8b64db94a7d3e6c008
Gabriel271088 <110410130+Gabriel271088@users.noreply.github.com>
2023-03-14 21:22:11 -0300

    Update valoracion_inventario.py

--- models/valoracion_inventario.py
+++ models/valoracion_inventario.py
@@ -5,5 +5,9 @@ from odoo import api, fields, models
 
 class ValoracionStock(models.Model):
     _inherit = "stock.valuation.layer"
-    
-    total_costo = fields.Float('Total valor a costo de reposición',related='product_id.standard_price')
+
+    total_costo = fields.Float('Total valor a costo de reposición', compute= '_compute_total') #,store= True ,related='product_id.standard_price'
+
+    def _compute_total(self):
+        for c in self:
+            c.total_costo = c.product_id.qty_available * c.product_id.with_company(c.company_id).replenishment_cost
